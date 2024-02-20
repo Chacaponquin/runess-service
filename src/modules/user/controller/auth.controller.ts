9417bcecd4efde1636ec469a882ebe42baeea3ff
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-02-20 12:44:58 -0500

    Add login route

--- src/modules/user/controller/auth.controller.ts
+++ src/modules/user/controller/auth.controller.ts
@@ -1,15 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { CreateUser } from '../use-cases';
+import { CreateUser, LoginUser } from '../use-cases';
 import { CreateUserDTO } from '../dto/create';
+import { CryptServices } from '@shared/services/crypt.service';
+import { ROUTES } from '@modules/app/constants';
+import { LoginUserDTO } from '../dto/login';
 
-@Controller('/auth')
+@Controller(ROUTES.AUTH.ROOT)
 export class AuthController {
-  constructor(private readonly userServices: UserService) {}
+  constructor(
+    private readonly userServices: UserService,
+    private readonly cryptServices: CryptServices
+  ) {}
 
-  @Post('/sign-up')
+  @Post(ROUTES.AUTH.SIGN_UP)
   async signUp(@Body() dto: CreateUserDTO): Promise<string> {
-    const useCase = new CreateUser(this.userServices);
+    const useCase = new CreateUser(this.userServices, this.cryptServices);
+    const token = await useCase.execute(dto);
+    return token;
+  }
+
+  @Post(ROUTES.AUTH.LOGIN)
+  async login(@Body() dto: LoginUserDTO): Promise<string> {
+    const useCase = new LoginUser(this.userServices, this.cryptServices);
     const token = await useCase.execute(dto);
     return token;
   }
--- src/modules/user/use-cases/index.ts
+++ src/modules/user/use-cases/index.ts
@@ -1,3 +1,4 @@
 import { CreateUser } from './CreateUser';
+import { LoginUser } from './LoginUser';
 
-export { CreateUser };
+export { CreateUser, LoginUser };
